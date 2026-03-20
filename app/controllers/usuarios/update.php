9760710e3a727bdc777842d44c43c6d9c2cf610d
Marvin Fergunson <marvinfergunson712@gmail.com>
2026-03-20 11:32:53 -0600

    fix: Se corrigio guardado de contrasena usuarios

--- app/controllers/usuarios/update.php
+++ app/controllers/usuarios/update.php
@@ -8,7 +8,41 @@ $password_user = $_POST['password_user'];
 $password_repeat = $_POST['password_repeat'];
 $id_usuario = $_POST['id_usuario'];
 
-if($password_user == $password_repeat){
+if($password_user == ""){
+    if($password_user == $password_repeat){
+    $password_user = password_hash($password_user, PASSWORD_DEFAULT);
+
+// consulta SQL
+$sql = ("UPDATE tb_usuarios 
+SET nombres=:nombres,
+email=:email,
+fyh_actualizacion=:fyh_actualizacion 
+WHERE id_usuario=:id_usuario");
+
+// preparar consulta
+$sentencia = $pdo->prepare($sql);
+
+// ejecutar consulta
+$sentencia->execute([
+    ':nombres' => $nombres,
+    ':email' => $email,
+    ':fyh_actualizacion' => $fechaHora,
+    ':id_usuario' => $id_usuario
+]);
+ session_start();
+    $_SESSION['mensaje'] = "Se actualizo Correctamente";
+    $_SESSION['icono'] = "success";
+    header('Location:' .$URL.'/usuarios');
+}else{
+    //echo "Error las contrasenas no son iguales";
+    session_start();
+    $_SESSION['mensaje'] = "Error las contrasenas no son iguales";
+    $_SESSION['icono'] = "error";
+    header('Location:' .$URL.'/usuarios/update.php?id='.$id_usuario);
+}
+
+}else{
+    if($password_user == $password_repeat){
     $password_user = password_hash($password_user, PASSWORD_DEFAULT);
 
 // consulta SQL
@@ -41,3 +75,6 @@ $sentencia->execute([
     $_SESSION['icono'] = "error";
     header('Location:' .$URL.'/usuarios/update.php?id='.$id_usuario);
 }
+
+}
+
