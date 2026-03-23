12242f64620c649070c5547503c2e6412fe59b10
Marvin Fergunson <marvinfergunson712@gmail.com>
2026-03-23 14:37:17 -0600

    fix: Crud - Categoria con Modals en el sistema

---
+++ categorias/index.php
@@ -0,0 +1,206 @@
+<?php
+include('../app/config.php');
+include('../layout/sesion.php');
+include('../layout/parte1.php');
+include('../app/controllers/categorias/listado_de_categorias');
+
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Listado de categorias
+                        <button type="button" class="btn btn-primary btn-sm" data-toggle="modal" data-target="#modal-creacion">
+                            <i class="fa fa-plus"></i>
+                            Agregar Nuevo
+                        </button>
+                    </h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+
+            <div class="row">
+                <div class="col-md-8">
+                    <div class="card card-outline card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Categorias Registradas</h3>
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                            <!-- /.card-tools -->
+                        </div>
+
+                        <!-- /.card-header -->
+                        <div class="card-body" style="display: block;">
+                            <table id="example1" class="table table-bordered table-striped">
+                                <thead>
+                                    <tr>
+                                        <th>
+                                            <center>Nro</center>
+                                        </th>
+                                        <th>
+                                            <center>Nombre Categoria</center>
+                                        </th>
+                                        <th>
+                                            <center>Acciones</center>
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <?php
+                                    $contador = 0;
+                                    foreach ($categorias_datos as $categorias_dato) {
+                                        $id_categoria = $categorias_dato['id_categorias']; ?>
+                                        <tr>
+                                            <td>
+                                                <center><?php echo $contador = $contador + 1; ?></center>
+                                            </td>
+                                            <td><?php echo $categorias_dato['nombre_categoria']; ?></td>
+                                            <td>
+                                                <center>
+                                                    <div class="btn-group">
+                                                        <a href="update.php?id=<?php echo $id_categoria; ?>" type="button" class="btn btn-success"><i class="fa fa-pencil-alt"></i>Editar</a>
+                                                    </div>
+                                                </center>
+                                            </td>
+                                        </tr>
+                                    <?php
+                                    }
+                                    ?>
+                                </tbody>
+                                <tfoot>
+                                    <tr>
+                                        <th>
+                                            <center>Nro</center>
+                                        </th>
+                                        <th>
+                                            <center>Nombre Categoria</center>
+                                        </th>
+                                        <th>
+                                            <center>Acciones</center>
+                                        </th>
+                                    </tr>
+                                </tfoot>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    </div>
+    <!-- /.content -->
+</div>
+<!-- /.content-wrapper -->
+
+<?php include('../layout/mensajes.php'); ?>
+<?php include('../layout/parte2.php'); ?>
+
+<script>
+    $(function() {
+        $("#example1").DataTable({
+            /* cambio de idiomas de datatable */
+            "pageLength": 5,
+            language: {
+                "emptyTable": "No hay información",
+                "decimal": "",
+                "info": "Mostrando _START_ a _END_ de _TOTAL_ categoria",
+                "infoEmpty": "Mostrando 0 to 0 of 0 Roles",
+                "infoFiltered": "(Filtrado de _MAX_ total categorias)",
+                "infoPostFix": "",
+                "thousands": ",",
+                "lengthMenu": "Mostrar _MENU_ categorias",
+                "loadingRecords": "Cargando...",
+                "processing": "Procesando...",
+                "search": "Buscador:",
+                "zeroRecords": "Sin resultados encontrados",
+                "paginate": {
+                    "first": "Primero",
+                    "last": "Ultimo",
+                    "next": "Siguiente",
+                    "previous": "Anterior"
+                }
+            },
+            /* fin de idiomas */
+            "responsive": true,
+            "lengthChange": true,
+            "autoWidth": false,
+            /* Ajuste de botones */
+            buttons: [{
+                    extend: 'collection',
+                    text: 'Reportes',
+                    orientation: 'landscape',
+                    buttons: [{
+                        text: 'Copiar',
+                        extend: 'copy'
+                    }, {
+                        extend: 'pdf',
+                    }, {
+                        extend: 'csv',
+                    }, {
+                        extend: 'excel',
+                    }, {
+                        text: 'Imprimir',
+                        extend: 'print'
+                    }]
+                },
+                {
+                    extend: 'colvis',
+                    text: 'Visol de columnas'
+                }
+            ],
+            /*Fin de ajuste de botones*/
+        }).buttons().container().appendTo('#example1_wrapper .col-md-6:eq(0)');
+    });
+</script>
+
+<!--MODAL PARA REGISTRAR CATEGORIAS-->
+
+<div class="modal fade" id="modal-creacion">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h4 class="modal-title">Creacion de Nueva Categoria</h4>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
+                <div class="row">
+                    <div class="col-md-12">
+                        <div class="form-froup">
+                            <label for="">Nombre de la categoria</label>
+                            <input type="text" class="form-control">
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="modal-footer justify-content-between">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary" id="btn_create">Guardar</button>
+                
+            </div>
+        </div>
+        <!-- /.modal-content -->
+    </div>
+    <!-- /.modal-dialog -->
+</div>
+<!-- /.modal- -->
+
+<script>
+    $('#btn_create').click(function(){
+        //alert("Guardar");
+        
+    });
+</script>
