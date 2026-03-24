8ac5d55e527933b2bb4c89d17d7313ddfbb6281a
Marvin Fergunson <marvinfergunson712@gmail.com>
2026-03-24 16:06:48 -0600

    fix: Formulario para el Registro de Productos

---
+++ almacen/create.php
@@ -0,0 +1,151 @@
+<?php
+include('../app/config.php');
+include('../layout/sesion.php');
+include('../layout/parte1.php');
+?>
+
+<!-- Content Wrapper. Contains page content -->
+<div class="content-wrapper">
+    <!-- Content Header (Page header) -->
+    <div class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-12">
+                    <h1 class="m-0">Registro de nueva producto</h1>
+                </div><!-- /.col -->
+            </div><!-- /.row -->
+        </div><!-- /.container-fluid -->
+    </div>
+    <!-- /.content-header -->
+    <!-- Main content -->
+    <div class="content">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+
+                    <div class="card card-primary">
+                        <div class="card-header">
+                            <h3 class="card-title">Llene los datos con cuidado</h3>
+
+                            <div class="card-tools">
+                                <button type="button" class="btn btn-tool" data-card-widget="collapse"><i class="fas fa-minus"></i>
+                                </button>
+                            </div>
+                            <!-- /.card-tools -->
+                        </div>
+                        <!-- /.card-header -->
+                        <div class="card-body" style="display: block;">
+                            <div class="row">
+                                <div class="col-md-12">
+                                    <form action="../app/controllers/roles/create.php" method="post">
+                                        <div class="row">
+                                            <div class="col-md-8">
+
+
+                                                <div class="row">
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label for="">Codigo:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label for="">Categoria:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label for="">Nombre:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                                <div class="row">
+                                                    <div class="col-md-4">
+                                                        <div class="form-group">
+                                                            <label for="">Usuario</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-8">
+
+                                                        <div class="form-group">
+                                                            <label for="">Descripcion:</label>
+                                                            <textarea name="" id="" cols="30" rows="2" class="form-control"></textarea>
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-4"></div>
+                                                </div>
+                                                <div class="row align-items-end">
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">Stock:</label>
+                                                            <input type="number" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">stock minimo:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">stock maximo:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">Precio compra:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">Precio venta:</label>
+                                                            <input type="text" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                    <div class="col-md-2">
+                                                        <div class="form-group">
+                                                            <label for="">Fecha de Ingreso:</label>
+                                                            <input type="date" class="form-control">
+                                                        </div>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                            <div class="col-md-3">
+                                                <div class="form-group">
+                                                    <label for="">Imagen del Producto</label>
+                                                    <input type="file" class="form-control">
+                                                </div>
+                                            </div>
+                                        </div>
+
+
+
+                                        <hr>
+                                        <div class="formgroup">
+                                            <a href="index.php" class="btn btn-secondary">Cancelar</a>
+                                            <button type="submit" class="btn btn-primary">Guardar</button>
+                                        </div>
+                                    </form>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+
+    </div><!-- /.container-fluid -->
+</div>
+
+<?php include('../layout/mensajes.php'); ?>
+<?php include('../layout/parte2.php'); ?>
